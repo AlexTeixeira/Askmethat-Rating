f827c16177e9c582b6c15589bf3e5effb943bc02
AlexTeixeira <teixeiraperso@gmail.com>
2017-06-13 16:20:21 +0200

    - Updating Karma conf - Updating package dependencies - Changing render variable names

--- karma.config.js
+++ karma.config.js
@@ -28,7 +28,6 @@ module.exports = function (config) {
             dir: 'coverage',
             subdir: '.',
             reporters: [
-                { type: 'html', dir: 'coverage/html' },
                 { type: 'lcov', dir: 'coverage/' }]
         },
         browsers: ["Chrome"],
